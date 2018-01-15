2b1a1eebf41b4eaae8d099b3167d2f84239763c8
LUU THIEN XUAN <luuthienxuan@gmail.com>
2018-01-15 16:31:35 +0900

    Add files via upload

--- Submit_file/model.py
+++ Submit_file/model.py
@@ -236,26 +236,3 @@ def train_model(model_chosen = 'Nvidia'):
 	return model
 
 train_model(model_chosen = 'Nvidia')
-
-"""
- VISUALIZING LOSS
-
-history_object = model.fit_generator(train_generator,\
-                                     samples_per_epoch = len(train_samples),\
-                                     validation_data   = validation_generator,\
-                                     nb_val_samples    = len(validation_samples),\
-                                     nb_epoch          = 5,\
-                                     verbose           = 1)
-
-### print the keys contained in the history object
-print(history_object.history.keys())
-
-### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
-"""
